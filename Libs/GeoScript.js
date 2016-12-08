91493a6f2f492e558998c56d3fb4100331f605c9
Alexe <Alexe@TTT-WORK>
2016-12-08 09:28:13 +0300

    Добавьте файлы проекта.

---
+++ Libs/GeoScript.js
@@ -0,0 +1,28 @@
+    navigator.geolocation.getCurrentPosition(updatePosition);
+
+    function updatePosition(pos) {
+
+        var position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+        var options = {
+            zoom: 15,
+            center: position,
+            mapTypeId: google.maps.MapTypeId.ROADMAP 
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), options);
+
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: "Это Вы!"
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: "Вы тут"
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+    }
